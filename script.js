f04fb76358538ddc67fdd11161506aca9af218e6
Elena Kovenkova <elenaluna.dance@gmail.com>
2024-07-14 19:38:30 +0300

    Task finished

---
+++ script.js
@@ -0,0 +1,20 @@
+function message() {
+    console.log('Я учу JavaScript!');
+}
+message();
+
+let img1 = document.getElementById('img1');
+
+let img2 = document.getElementById('img2');
+
+let previous = document.getElementById('previous');
+
+let next = document.getElementById('next');
+
+function function1() {
+    img1.src = 'assets/images/patagoniya1.jpg';
+}
+
+function function2() {
+    img1.src = 'assets/images/patagonia2.jpg';
+}
